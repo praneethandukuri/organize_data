005f5ad72732bb601c61daee1628075fe97b90c8
praneethandukuri <152866355+praneethandukuri@users.noreply.github.com>
2024-12-23 15:25:26 +0530

    adds people data to organize_data.js

--- organize_data.js
+++ organize_data.js
@@ -1,1 +1,91 @@
+const people = [
+  {
+    name: "Rahul",
+    age: 30,
+    profession: "software engineer",
+    city: "Pune",
+    hobbies: ["playing chess", "gardening"],
+    pets: [
+      {
+        name: "Max",
+        type: "Dog",
+        breed: "golden retriever",
+        age: 4,
+        vaccinated: true,
+        favouriteActivity: "playing fetch in the park",
+      },
+    ],
+    isEmployed: true,
+    ownsCar: true,
+    studied: ["Computer Science"],
+  },
 
+  {
+    name: "Ananya",
+    age: 30,
+    profession: "cook",
+    city: "Bangalore",
+    hobbies: ["cooking", "experimenting with Italian recipes"],
+    pets: [
+      {
+        name: "Kiwi",
+        type: "Parrot",
+        vaccinated: true,
+        traits: ["knows over 20 phrases", "mimics ananya's voice"],
+      },
+    ],
+    isEmployed: false,
+    ownsCar: false,
+    studied: ["Computer Science", "Graphic Design"],
+  },
+
+  {
+    name: "Ramesh",
+    age: 45,
+    profession: "Business Owner",
+    city: "Jaipur",
+    hobbies: ["Gardening", "Reading historical fiction"],
+    pets: [
+      {
+        name: "Bella",
+        type: "Cat",
+        breed: "Persian",
+        age: 3,
+        vaccinated: true,
+        favouriteActivity: "Lounging in the sun",
+      },
+      {
+        name: "Leo",
+        type: "Cat",
+        breed: "Persian",
+        age: 3,
+        vaccinated: true,
+        favouriteActivity: "Lounging in the sun",
+      },
+    ],
+    isEmployed: true,
+    ownsCar: true,
+    studied: [],
+  },
+
+  {
+    name: "Kavya",
+    age: 28,
+    profession: "Dancer",
+    city: "Chennai",
+    hobbies: ["modern fantasy novels", "binge-watching sci-fi shows"],
+    pets: [
+      {
+        name: "Snowy",
+        type: "Rabbit",
+        age: 2,
+        vaccinated: true,
+        favouriteActivity:
+          "hopping around her backyard and nibbling on carrots",
+      },
+    ],
+    isEmployed: false,
+    ownsCar: false,
+    studied: [],
+  },
+];
